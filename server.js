36dd778bedccbdf6a47dd6e6cb40906cc988eb4f
mohstarsos1 <mohammad.ghasemi68@gmail.com>
2025-01-20 18:42:46 +0330

    fix path for load bootstrap files

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/',(req,res)=>{
     res.render("pages/home",{title:".: POMOTODO :."});
