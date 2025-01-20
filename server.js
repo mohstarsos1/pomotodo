95b8b5b8593a618fd8a4feb1a43592187c15512b
mohstarsos1 <mohammad.ghasemi68@gmail.com>
2025-01-20 17:36:28 +0330

    add template files

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("hello");
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'ejs');
+
+app.get('/',(req,res)=>{
+    res.render("pages/home",{title:".: POMOTODO :."});
+})
+
+const port = 5000;
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
